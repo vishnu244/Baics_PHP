9136af2bfe5897731c9c53e50ed451e9f1a4b73f
vishnu244 <vishnunali244@gmail.com>
2022-08-09 10:15:04 +0530

    [Vishnu]-Checking whether the Given Number is Even or Odd

---
+++ EvenOdd.php
@@ -0,0 +1,16 @@
+<?php 
+
+$x = readline("Enter a Number ") ; 
+
+function EvenOdd($x){ 
+    if($x % 2 == 0){ 
+        echo $x." is an Even Number";  
+    } 
+    else{ 
+        echo $x." is an Odd Number";  
+    } 
+}  
+EvenOdd($x) 
+
+?> 
+
