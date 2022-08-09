8ec66f1ec2a4c15135f581e8df3f6e2ef4dcc6cb
vishnu244 <vishnunali244@gmail.com>
2022-08-09 11:48:46 +0530

    [Vishnu]-Finding the Quotient and Remainder for given Dividend

---
+++ QuotientRemainder.php
@@ -0,0 +1,11 @@
+<?php
+$Dividend = readline("Enter the Values of Dividend : ");
+$Divisor = readline("Enter the Values of Divisor : ");
+
+$quotient = $Dividend / $Divisor;
+$remainder = $Dividend % $Divisor;
+
+echo "Value of Quotient : $quotient \n";
+echo "Value of remainder : $remainder"; 
+
+?>
