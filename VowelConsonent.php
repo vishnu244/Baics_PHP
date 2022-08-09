b6626c371903bcacd4f71ee238b033e7577cdde2
vishnu244 <vishnunali244@gmail.com>
2022-08-09 11:00:11 +0530

    [Vishnu]-Checking whether the given Alphabet is Vowel or Consonent

---
+++ VowelConsonent.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Please Enter an Alphabet (A-Z or a-z)\n ";
+
+$x = readline("Enter an alphabet : ");
+if ($x == 'A' || $x == 'a')
+    echo "Entered Alphabet is Vowel";
+elseif ($x == 'E' || $x == 'e')
+    echo "Entered Alphabet is Vowel";
+elseif ($x == 'I' || $x == 'i')
+    echo "Entered Alphabet is Vowel";
+elseif ($x == 'O' || $x == 'o')
+    echo "Entered Alphabet is Vowel";
+elseif ($x == 'U' || $x == 'u')
+    echo "Entered Alphabet is Vowel";
+else
+    echo "Entered Alphabet is a Consonent";
+              
+
+?>
